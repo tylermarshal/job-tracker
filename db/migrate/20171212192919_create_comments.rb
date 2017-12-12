d795c2194c84d68b1ec08f8d98bc845544ac4c86
TMad <madsen.tyler@gmail.com>
2017-12-12 13:16:57 -0700

    add basic comment functionality

--- db/migrate/20171212192919_create_comments.rb
+++ db/migrate/20171212192919_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.1]
     create_table :comments do |t|
       t.string :author_name
       t.text :body
+      t.timestamps null: false
       t.references :job, foreign_key: true
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20171212192919) do
   create_table "comments", force: :cascade do |t|
     t.string "author_name"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "job_id"
     t.index ["job_id"], name: "index_comments_on_job_id"
   end
