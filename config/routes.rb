c9a440bf3d4c6b5635c432997339317fd2a55b0c
TMad <madsen.tyler@gmail.com>
2017-12-12 15:28:37 -0700

    add dashboard controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,11 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  resources :jobs, only: [:index]
+  resources :company do
+    resources :contacts
+  end
+
+  resources :dashboard, only[:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
