d414395069ae1fa98d1b269e2223dcd86d8b6085
TMad <madsen.tyler@gmail.com>
2017-12-13 19:53:08 -0700

    add destory dependencies to company and jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,5 +2,5 @@ class Job < ApplicationRecord
   validates :title, :level_of_interest, :city, presence: true
   belongs_to :company
   belongs_to :category
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
